7d5d9c9f9cb6a1b21acbe0b72a25a323ea7f0627
Augustin Bégué <augustin.begue@epita.fr>
2022-06-30 15:08:57 +0200

    feat(styles): added rapjeu theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+  daisyui: {
+    themes: [
+      {
+        rapjeuLight: {
+          "primary": "#f8c800",
+          "secondary": "#0034d3",
+          "accent": "#e23141",
+          "neutral": "#e5e7eb",
+          "base-100": "#f3f4f6",
+          "info": "#0034d3",
+          "success": "#16a34a",
+          "warning": "#FBBD23",
+          "error": "#e23141",
+        }
+      }
+    ]
+  }
+};
