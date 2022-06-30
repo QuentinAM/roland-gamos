95bb9a652b2f62d57960089c6b6b1d550948f1f3
Augustin Bégué <augustin.begue@epita.fr>
2022-06-30 13:33:58 +0200

    feat(chore): improved folder structures

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import Hero from "../components/Hero/index.svelte";
+	import Hero from '../lib/components/Hero/index.svelte';
 
-    const createRoom = () => {
-        // Generate a random room id
-        const roomId: string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        window.location.href = `/lobby?id=${roomId.substring(0, 20)}`;
-    }
+	const createRoom = () => {
+		// Generate a random room id
+		const roomId: string =
+			Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+		window.location.href = `/lobby?id=${roomId.substring(0, 20)}`;
+	};
 </script>
 
-<Hero {createRoom}/>
+<Hero {createRoom} />
