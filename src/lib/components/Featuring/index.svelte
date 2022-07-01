d314a27546718274033131d5035f7667d8046220
Augustin Bégué <augustin.begue@epita.fr>
2022-07-01 10:35:38 +0200

    feat(game): added tracks

--- src/lib/components/Featuring/index.svelte
+++ src/lib/components/Featuring/index.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
-    export let img_src: string = '';
-    export let title: string = '';
-    export let release_date: string = '';
-    export let audio_src: string = '';
-    export let artist1_src: string = '';
-    export let artist2_src: string = '';
+	export let imgUrl: string;
+	export let title: string;
+	export let releaseDate: string;
+	export let audioUrl: string;
+	export let artist1ImageUrl: string;
+	export let artist2ImageUrl: string;
 </script>
 
 <div class="card card-compact w-96 bg-base-100 shadow-xl">
-    <figure>
-        <img src={img_src} alt="Shoes"/>
-    </figure>
-    <div class="card-body">
-        <h2 class="card-title">{title}</h2>
-        <p>{release_date}</p>
-    </div>
-    <audio
-        class="w-full"
-        src={audio_src}
-        controls>
-        <track kind="captions"/>
-    </audio>
-    <div class="avatar-group absolute bottom-10 right-0 -space-x-3">
-        <div class="avatar">
-            <div class="w-12">
-                <img src={artist1_src} alt='artist1'/>
-            </div>
-        </div>
-        <div class="avatar">
-            <div class="w-12">
-                <img src={artist2_src} alt='artist2'/>
-            </div>
-        </div>
-    </div>
+	<figure>
+		<img src={imgUrl} alt="Shoes" />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{releaseDate}</p>
+	</div>
+	{#if audioUrl}
+		<audio class="w-full" src={audioUrl} controls>
+			<track kind="captions" />
+		</audio>
+	{/if}
+	<div class="avatar-group absolute bottom-10 right-0 -space-x-3">
+		<div class="avatar">
+			<div class="w-12">
+				<img src={artist1ImageUrl} alt="artist1" />
+			</div>
+		</div>
+		<div class="avatar">
+			<div class="w-12">
+				<img src={artist2ImageUrl} alt="artist2" />
+			</div>
+		</div>
+	</div>
 </div>
--- src/routes/room/[roomId]/game.svelte
+++ src/routes/room/[roomId]/game.svelte
@@ -4,8 +4,8 @@
 	import Fa from 'svelte-fa';
 	import { onMount } from 'svelte';
 	import { faArrowsRotate, faCheck, faClock, faMusic } from '@fortawesome/free-solid-svg-icons';
+	import Featuring from '$lib/components/Featuring/index.svelte';
 	import type { GuessingMessage, GuessMessage } from 'src/websocketserver/wstypes';
-	import type { Unsubscriber } from 'svelte/store';
 
 	let turnDuration = 30_000;
 	let currentTime = Date.now();
@@ -14,6 +14,7 @@
 	$: currentTurn = $room?.currentTurn;
 	$: currentGuess = $room?.currentGuess;
 	$: currentArtist = $room?.enteredArtists[$room?.currentTurn - 1];
+	$: currentTrack = $room?.currentTrack;
 	$: currentPlayerIndex = $room?.currentPlayerIndex;
 	$: currentPlayerHasAttemptedGuess = $room?.currentPlayerHasAttemptedGuess;
 	$: currentPlayerHasGuessed = $room?.currentPlayerHasGuessed;
@@ -26,8 +27,9 @@
 
 	$: isCurrentPlayer =
 		players && currentPlayerIndex != undefined
-			? players[currentPlayerIndex].userId === $player?.userId
+			? players[currentPlayerIndex]?.userId === $player?.userId
 			: false;
+	$: isGameOver = $room?.players?.length === 1;
 
 	let guess = '';
 	// Send the final guess attempt to the server
@@ -104,96 +106,108 @@
 </script>
 
 <div class="hero min-h-screen">
-	<div class="hero-content flex flex-col justify-start h-full w-full">
-		<div class="stats shadow">
-			<div class="stat bg-primary">
-				<div class="stat-figure text-primary-content" />
-				<div class="stat-title text-primary-content">Tour</div>
-				<div class="stat-value text-primary-content inline-flex">
-					{currentTurn}
-					<span class="ml-4 mt-2">
-						<Fa icon={faArrowsRotate} size={'xs'} />
-					</span>
+	<div class="hero-content flex flex-row justify-start items-start h-full w-full">
+		<div class="flex flex-col w-full justify-start">
+			<div class="stats shadow">
+				<div class="stat bg-primary">
+					<div class="stat-figure text-primary-content" />
+					<div class="stat-title text-primary-content">Tour</div>
+					<div class="stat-value text-primary-content inline-flex">
+						{currentTurn}
+						<span class="ml-4 mt-2">
+							<Fa icon={faArrowsRotate} size={'xs'} />
+						</span>
+					</div>
+				</div>
+				<div class="stat bg-secondary">
+					<div class="stat-title text-secondary-content">Temps Restant</div>
+					<div class="stat-value text-secondary-content inline-flex">
+						{remainingTimeSeconds}s
+						<span class="ml-4 mt-2">
+							<Fa icon={faClock} size={'xs'} />
+						</span>
+					</div>
+				</div>
+				<div class="stat bg-accent">
+					<div class="stat-title text-accent-content">Artiste</div>
+					<div class="stat-value text-accent-content inline-flex">
+						{currentArtist?.name ?? 'Aucun'}
+						<span class="ml-4 mt-2">
+							<Fa icon={faMusic} size={'xs'} />
+						</span>
+					</div>
 				</div>
 			</div>
-			<div class="stat bg-secondary">
-				<div class="stat-title text-secondary-content">Temps Restant</div>
-				<div class="stat-value text-secondary-content inline-flex">
-					{remainingTimeSeconds}s
-					<span class="ml-4 mt-2">
-						<Fa icon={faClock} size={'xs'} />
-					</span>
+
+			{#if players}
+				<div class="grid grid-cols-4 grid-flow-row gap-8 mt-16 w-full">
+					{#each players as p, i}
+						<div class="flex flex-col justify-center items-center">
+							<p class="font-semibold">{p.username}</p>
+							<div
+								class="w-[50%]  h-2 rounded"
+								class:bg-primary={currentPlayerIndex === i}
+								class:bg-base-300={currentPlayerIndex !== i}
+							/>
+							<div
+								class="w-[40%] h-16 rounded-b shadow-lg"
+								class:bg-primary={currentPlayerIndex === i}
+								class:bg-base-300={currentPlayerIndex !== i}
+							>
+								{#if currentPlayerIndex === i}
+									<div
+										class="p-2 m-2 text-2xs text-center rounded"
+										class:bg-base-100={!currentPlayerHasGuessed && remainingTimeSeconds > 0}
+										class:bg-success={currentPlayerHasGuessed}
+										class:bg-error={(!currentPlayerHasGuessed && remainingTimeSeconds <= 0) ||
+											(currentPlayerHasAttemptedGuess && !currentPlayerHasGuessed)}
+									>
+										{currentGuess || '‎'}
+									</div>
+								{/if}
+							</div>
+						</div>
+					{/each}
 				</div>
-			</div>
-			<div class="stat bg-accent">
-				<div class="stat-title text-accent-content">Artiste</div>
-				<div class="stat-value text-accent-content inline-flex">
-					{currentArtist?.name ?? 'Aucun'}
-					<span class="ml-4 mt-2">
-						<Fa icon={faMusic} size={'xs'} />
-					</span>
-				</div>
-			</div>
+			{/if}
 		</div>
 
-		{#if players}
-			<div class="grid grid-cols-4 grid-flow-row gap-8 mt-16 w-full">
-				{#each players as p, i}
-					<div class="flex flex-col justify-center items-center">
-						<p class="font-semibold">{p.username}</p>
-						<div
-							class="w-[50%]  h-2 rounded"
-							class:bg-primary={currentPlayerIndex === i}
-							class:bg-base-300={currentPlayerIndex !== i}
-						/>
-						<div
-							class="w-[40%] h-16 rounded-b shadow-lg"
-							class:bg-primary={currentPlayerIndex === i}
-							class:bg-base-300={currentPlayerIndex !== i}
-						>
-							{#if currentPlayerIndex === i}
-								<div
-									class="p-2 m-2 text-2xs text-center rounded"
-									class:bg-base-100={!currentPlayerHasGuessed && remainingTimeSeconds > 0}
-									class:bg-success={currentPlayerHasGuessed}
-									class:bg-error={(!currentPlayerHasGuessed && remainingTimeSeconds <= 0) ||
-										(currentPlayerHasAttemptedGuess && !currentPlayerHasGuessed)}
-								>
-									{currentGuess || '‎'}
-								</div>
-							{/if}
-						</div>
-					</div>
-				{/each}
-			</div>
-		{/if}
-
-		{#if isCurrentPlayer}
-			<div class="absolute bottom-0 p-16 w-full">
-				<div class="card shadow-lg">
-					<div class="card-body">
-						<div class="form-control flex-row">
-							<input
-								class="input input-primary w-full rounded-r-none"
-								type="text"
-								placeholder="Entre un artiste."
-								bind:value={guess}
-								on:input={guessing}
-								disabled={currentPlayerHasAttemptedGuess}
-							/>
-							<button
-								class="btn btn-success rounded-l-none"
-								on:click={submitGuess}
-								disabled={currentPlayerHasAttemptedGuess}
-							>
-								<span class="mr-2">
-									<Fa icon={faCheck} size={'lg'} />
-								</span> Valider
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
+		{#if currentTrack}
+			<Featuring
+				audioUrl={currentTrack.previewUrl}
+				artist1ImageUrl={currentArtist?.imageUrl ?? ''}
+				artist2ImageUrl={currentTrack.artist.imageUrl}
+				imgUrl={currentTrack.trackImage}
+				releaseDate={currentTrack.releaseDate}
+				title={currentTrack.name}
+			/>
 		{/if}
 	</div>
 </div>
+{#if isCurrentPlayer}
+	<div class="absolute bottom-0 p-16 w-full">
+		<div class="card shadow-lg">
+			<div class="card-body">
+				<div class="form-control flex-row">
+					<input
+						class="input input-primary w-full rounded-r-none"
+						type="text"
+						placeholder="Entre un artiste."
+						bind:value={guess}
+						on:input={guessing}
+						disabled={currentPlayerHasAttemptedGuess}
+					/>
+					<button
+						class="btn btn-success rounded-l-none"
+						on:click={submitGuess}
+						disabled={currentPlayerHasAttemptedGuess}
+					>
+						<span class="mr-2">
+							<Fa icon={faCheck} size={'lg'} />
+						</span> Valider
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
