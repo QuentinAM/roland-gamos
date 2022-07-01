36bc1e5be6c629210cb70151f26220f62084cb92
Augustin Bégué <augustin.begue@epita.fr>
2022-07-01 09:06:44 +0200

    feat(apI): use vite .env implementation

--- src/routes/api/utils.ts
+++ src/routes/api/utils.ts
@@ -1,25 +1,23 @@
-import * as secret from './.env.json';
 
 export async function GetToken() {
-    const CLIENT_ID = secret.CLIENT_ID;
-    const CLIENT_SECRET = secret.CLIENT_SECRET;
+    const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+    const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 
     const response = await fetch(`https://accounts.spotify.com/api/token`, {
-                        method: 'POST',
-                        headers: {
-                            'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')),
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                        body: new URLSearchParams({
-                            'grant_type': 'client_credentials'
-                        })
-                    });
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')),
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            'grant_type': 'client_credentials'
+        })
+    });
     const data = await response.json();
     return data.access_token;
 }
 
-export async function GetArtistPicture(request: string, token: string | null)
-{
+export async function GetArtistPicture(request: string, token: string | null) {
     const response = await fetch(request, {
         method: 'GET',
         headers: {
