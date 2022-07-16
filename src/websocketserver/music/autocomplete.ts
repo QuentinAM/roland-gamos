d2fb18f1b3839e014256396072574b681edfc29a
Augustin Bégué <augustin.begue@epita.fr>
2022-07-16 10:20:41 +0200

    fix(autocomplete): fixed a crash on empty response

--- src/websocketserver/music/autocomplete.ts
+++ src/websocketserver/music/autocomplete.ts
@@ -6,7 +6,7 @@ export async function autoComplete(input: string): Promise<Artist[]> {
     if (!input) return [];
 
     // Get both artists
-    let { artists }: {artists: any[]}= await autoCompleteEndpoint(input, spToken);
+    let { artists }: { artists: any[] } = await autoCompleteEndpoint(input, spToken);
 
     // Filter list
     // If two arists have the same name, take the one with more followers
@@ -37,7 +37,7 @@ export async function autoComplete(input: string): Promise<Artist[]> {
     return artists;
 }
 
-function FormatName(name: string){
+function FormatName(name: string) {
     // Lowercase
     name = name.toLowerCase();
 
@@ -51,8 +51,8 @@ function FormatName(name: string){
 }
 
 async function autoCompleteEndpoint(input: string, token: string | null): Promise<any> {
-    let url =  encodeURI(`https://api.spotify.com/v1/search?limit=3&market=FR&type=artist&q=${input}`);
-    
+    let url = encodeURI(`https://api.spotify.com/v1/search?limit=3&market=FR&type=artist&q=${input}`);
+
     const response = await fetch(url, {
         method: 'GET',
         headers: {
@@ -70,6 +70,6 @@ async function autoCompleteEndpoint(input: string, token: string | null): Promis
     }
 
     return {
-        artists: data.artists.items ? data.artists.items : []
+        artists: data?.artists?.items ? data.artists.items : []
     }
 }
